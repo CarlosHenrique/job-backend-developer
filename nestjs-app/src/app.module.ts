b46dbc560543eff6810e1872185bd42b26371213
Carlos Souza <carlossouzap@outlook.com>
2024-03-02 19:42:41 -0300

    style: applying eslint

--- nestjs-app/src/app.module.ts
+++ nestjs-app/src/app.module.ts
@@ -4,18 +4,21 @@ import { MoviereviewsModule } from './moviereviews/moviereviews.module';
 import { OmdbModule } from './omdb/omdb.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_DOCKER_PORT),
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    autoLoadEntities: true,
-    synchronize: true
-  }),
-    MoviereviewsModule, OmdbModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_DOCKER_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    MoviereviewsModule,
+    OmdbModule,
+  ],
   controllers: [],
   providers: [],
 })
-export class AppModule { }
+export class AppModule {}
