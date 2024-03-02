2ca489edb61cff95b76c2e17c879b71cfc5e889c
Carlos Souza <carlossouzap@outlook.com>
2024-03-02 19:41:35 -0300

    feat: add return type on functions

--- nestjs-app/src/moviereviews/moviereviews.controller.ts
+++ nestjs-app/src/moviereviews/moviereviews.controller.ts
@@ -1,24 +1,24 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { CreateMovieReviewDomainDto } from './dtos/create-movie-review.dto';
-import { MoviereviewsService } from './moviereviews.service';
+import { GetMovieReviewsFilterDto } from './dtos/get-movie-reviews.dto';
+import { MoviereviewsService, PageResult } from './moviereviews.service';
+import { MovieReview } from './entities/movie-review';
 
 @Controller('movie-reviews')
 export class MoviereviewsController {
-    constructor(
-        private readonly movieReviewService: MoviereviewsService,
+  constructor(private readonly movieReviewService: MoviereviewsService) {}
 
-    ) { }
+  @Post()
+  createReview(
+    @Body() createMovieReviewDto: CreateMovieReviewDomainDto,
+  ): Promise<MovieReview> {
+    return this.movieReviewService.createReview(createMovieReviewDto);
+  }
 
-    @Post()
-    createReview(
-
-        @Body() createMovieReviewDto: CreateMovieReviewDomainDto
-    ) {
-        return this.movieReviewService.createReview(createMovieReviewDto)
-    }
-
-    @Get()
-    getReviews() {
-        return this.movieReviewService.getReviews()
-    }
+  @Get()
+  getReviews(
+    @Query() filter: GetMovieReviewsFilterDto,
+  ): Promise<PageResult<MovieReview>> {
+    return this.movieReviewService.getReviews(filter);
+  }
 }
