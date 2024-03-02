4e3964914a16739fd4735effcbc92b1f22cf34ce
Carlos Souza <carlossouzap@outlook.com>
2024-03-02 19:41:03 -0300

    feat: add function to construct query based on filter applied

--- nestjs-app/src/moviereviews/moviereviews.service.ts
+++ nestjs-app/src/moviereviews/moviereviews.service.ts
@@ -2,50 +2,74 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { randomUUID } from 'crypto';
 import { OmdbService } from 'src/omdb/omdb.service';
-import { Repository } from 'typeorm';
+import { Repository, SelectQueryBuilder } from 'typeorm';
 import { CreateMovieReviewDomainDto } from './dtos/create-movie-review.dto';
+import { GetMovieReviewsFilterDto } from './dtos/get-movie-reviews.dto';
 import { MovieReview } from './entities/movie-review';
 import { mapToMovieReview } from './mappers/map-movieapi-to-moviereview';
 
-
 export interface PageResult<T> {
-    data: T[]
-    total: number
+  data: T[];
+  total: number;
 }
 
 @Injectable()
 export class MoviereviewsService {
-    constructor(
-        @InjectRepository(MovieReview)
-        private movieReviewRepository: Repository<MovieReview>,
-        private readonly omdbService: OmdbService
-    ) { }
+  constructor(
+    @InjectRepository(MovieReview)
+    private movieReviewRepository: Repository<MovieReview>,
+    private readonly omdbService: OmdbService,
+  ) {}
 
-    async createReview(createReviewArgs: CreateMovieReviewDomainDto): Promise<MovieReview> {
-        try {
-            const { title, notes } = createReviewArgs
-            console.log(createReviewArgs)
-            console.log(title)
+  async createReview(
+    createReviewArgs: CreateMovieReviewDomainDto,
+  ): Promise<MovieReview> {
+    try {
+      const { title, notes } = createReviewArgs;
 
-            const omdbFoundData = await this.omdbService.searchMovies(title)
-            const mappedReview = mapToMovieReview(omdbFoundData, notes)
+      const omdbFoundData = await this.omdbService.searchMovies(title);
+      const mappedReview = mapToMovieReview(omdbFoundData, notes);
 
-            const created = await this.movieReviewRepository.save({ ...mappedReview, id: randomUUID() })
-            console.log(created.id)
-            return created
+      const created = await this.movieReviewRepository.save({
+        ...mappedReview,
+        id: randomUUID(),
+      });
 
-        } catch (err) {
-            throw new Error('Error creating review from database')
-        }
+      return created;
+    } catch (err) {
+      throw new Error('Error creating review from database');
+    }
+  }
 
+  async getReviews(
+    filter: GetMovieReviewsFilterDto,
+  ): Promise<PageResult<MovieReview>> {
+    let query = this.movieReviewRepository.createQueryBuilder('movieReviews');
+    query = this.constructWhere(query, filter);
+    const [data, total] = await query.getManyAndCount();
+
+    return { data, total };
+  }
+
+  private constructWhere(
+    query: SelectQueryBuilder<MovieReview>,
+    filter: GetMovieReviewsFilterDto,
+  ): SelectQueryBuilder<MovieReview> {
+    const { sortRating, sortRelease, search } = filter;
+
+    if (search) {
+      query.andWhere(
+        'LOWER(movieReviews.title) LIKE LOWER(:search) OR LOWER(movieReviews.actors) LIKE LOWER(:search) OR LOWER(movieReviews.directors) LIKE LOWER(:search)',
+        { search: `%${search}%` },
+      );
+    }
+    if (sortRelease) {
+      query.addOrderBy('movieReviews.released', sortRelease);
     }
 
-    async getReviews(): Promise<PageResult<MovieReview>> {
-        const [data, total] = await this.movieReviewRepository.findAndCount(
-
-        )
-
-
-        return { data, total }
+    if (sortRating) {
+      query.addOrderBy('movieReviews.imdbRating', sortRating);
     }
+    return query;
+  }
 }
