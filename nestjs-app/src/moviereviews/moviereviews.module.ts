47dc007f3850aff20a12c30ee33a93f2dd1f1da3
Carlos Souza <carlossouzap@outlook.com>
2024-03-04 01:01:51 -0300

    fix: src in imports

--- nestjs-app/src/moviereviews/moviereviews.module.ts
+++ nestjs-app/src/moviereviews/moviereviews.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { OmdbModule } from 'src/omdb/omdb.module';
+import { OmdbModule } from '../omdb/omdb.module';
 import { MovieReview } from './entities/movie-review';
 import { MoviereviewsController } from './moviereviews.controller';
 import { MoviereviewsService } from './moviereviews.service';
 @Module({
-  imports: [TypeOrmModule.forFeature([MovieReview]), OmdbModule],
-  controllers: [MoviereviewsController],
-  providers: [MoviereviewsService],
+    imports: [TypeOrmModule.forFeature([MovieReview]), OmdbModule],
+    controllers: [MoviereviewsController],
+    providers: [MoviereviewsService],
 })
 export class MoviereviewsModule {}
--- nestjs-app/src/moviereviews/moviereviews.service.ts
+++ nestjs-app/src/moviereviews/moviereviews.service.ts
@@ -1,139 +1,157 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { randomUUID } from 'crypto';
-import { OmdbService } from 'src/omdb/omdb.service';
+import { OmdbService } from '../omdb/omdb.service';
 import { Repository, SelectQueryBuilder } from 'typeorm';
 import { CreateMovieReviewDomainDto } from './dtos/create-movie-review.dto';
 import { GetMovieReviewsQueryDto } from './dtos/get-movie-reviews.dto';
 import { MovieReview } from './entities/movie-review';
 import { mapToMovieReview } from './mappers/map-movieapi-to-moviereview';
-import { RepositoryGenericError } from 'src/utils/repository-generic-error';
+import { RepositoryGenericError } from '../utils/repository-generic-error';
 import { UpdateMovieReviewDto } from './dtos/update-movie-review.dto';
 
 export interface PageResult<T> {
-  data: T[];
-  total: number;
-  totalPages?: number;
-  currentPage?: number;
+    data: T[];
+    total: number;
+    totalPages: number;
+    currentPage: number;
 }
 
 @Injectable()
 export class MoviereviewsService {
-  constructor(
-    @InjectRepository(MovieReview)
-    private movieReviewRepository: Repository<MovieReview>,
-    private readonly omdbService: OmdbService,
-  ) {}
+    constructor(
+        @InjectRepository(MovieReview)
+        private movieReviewRepository: Repository<MovieReview>,
+        private readonly omdbService: OmdbService,
+    ) {}
 
-  async createReview(
-    createReviewArgs: CreateMovieReviewDomainDto,
-  ): Promise<MovieReview> {
-    try {
-      const { title, notes } = createReviewArgs;
+    async createReview(
+        createReviewArgs: CreateMovieReviewDomainDto,
+    ): Promise<MovieReview> {
+        try {
+            const { title, notes } = createReviewArgs;
 
-      const omdbFoundData = await this.omdbService.searchMovies(title);
-      const mappedReview = mapToMovieReview(omdbFoundData, notes);
+            const omdbFoundData = await this.omdbService.searchMovies(title);
 
-      const created = await this.movieReviewRepository.save({
-        ...mappedReview,
-        id: randomUUID(),
-      });
+            const mappedReview = mapToMovieReview(omdbFoundData, notes);
 
-      return created;
-    } catch (err) {
-      throw new RepositoryGenericError('Error creating review from database');
-    }
-  }
+            const created = await this.movieReviewRepository.save({
+                ...mappedReview,
+                id: randomUUID(),
+            });
 
-  async getReviews(
-    queryDto: GetMovieReviewsQueryDto,
-  ): Promise<PageResult<MovieReview>> {
-    try {
-      const { pageNumber, pageSize } = queryDto;
-
-      let query = this.movieReviewRepository.createQueryBuilder('movieReviews');
-
-      query = this.constructWhere(query, queryDto);
-
-      query.skip(pageSize * (pageNumber - 1)).take(pageSize);
-
-      const [data, total] = await query.getManyAndCount();
-
-      return {
-        data,
-        total,
-        totalPages: Math.ceil(total / pageSize),
-        currentPage: pageNumber,
-      };
-    } catch (err) {
-      throw new RepositoryGenericError('Error getting reviews from database');
-    }
-  }
-
-  async getReviewById(id: string): Promise<MovieReview> {
-    try {
-      const found = await this.movieReviewRepository.findOneBy({ id });
-      if (!found) {
-        throw new RepositoryGenericError('Error getting review from database');
-      }
-      return found;
-    } catch (err) {
-      throw new RepositoryGenericError('Error getting review from database');
-    }
-  }
-
-  async deleteReviewById(id: string): Promise<void> {
-    try {
-      const result = await this.movieReviewRepository.delete({ id });
-      if (result.affected === 0) {
-        throw new RepositoryGenericError('Error deleting review from database');
-      }
-      return;
-    } catch (err) {
-      throw new RepositoryGenericError('Error deleting review from database');
-    }
-  }
-
-  async updateMovieReviewNotes(
-    id: string,
-    data: UpdateMovieReviewDto,
-  ): Promise<MovieReview> {
-    try {
-      const { notes } = data;
-
-      const found = await this.movieReviewRepository.findOne({
-        where: { id },
-      });
-      if (!found) {
-        throw new RepositoryGenericError('Review not found on database');
-      }
-      found.notes = notes;
-
-      return await this.movieReviewRepository.save(found);
-    } catch (err) {
-      throw new RepositoryGenericError('Error updating review from database');
-    }
-  }
-
-  private constructWhere(
-    query: SelectQueryBuilder<MovieReview>,
-    dto: GetMovieReviewsQueryDto,
-  ): SelectQueryBuilder<MovieReview> {
-    const { sortRating, sortRelease, search } = dto;
-
-    if (search) {
-      query.andWhere(
-        'LOWER(movieReviews.title) LIKE LOWER(:search) OR LOWER(movieReviews.actors) LIKE LOWER(:search) OR LOWER(movieReviews.directors) LIKE LOWER(:search)',
-        { search: `%${search}%` },
-      );
-    }
-    if (sortRelease) {
-      query.addOrderBy('movieReviews.released', sortRelease);
+            return created;
+        } catch (err) {
+            throw new RepositoryGenericError(
+                'Error creating review from database',
+            );
+        }
     }
 
-    if (sortRating) {
-      query.addOrderBy('movieReviews.imdbRating', sortRating);
+    async getReviews(
+        queryDto: GetMovieReviewsQueryDto,
+    ): Promise<PageResult<MovieReview>> {
+        try {
+            const { pageNumber, pageSize } = queryDto;
+
+            let query =
+                this.movieReviewRepository.createQueryBuilder('movieReviews');
+
+            query = this.constructWhere(query, queryDto);
+
+            query.skip(pageSize * (pageNumber - 1)).take(pageSize);
+
+            const [data, total] = await query.getManyAndCount();
+
+            return {
+                data,
+                total,
+                totalPages: Math.ceil(total / pageSize),
+                currentPage: pageNumber,
+            };
+        } catch (err) {
+            throw new RepositoryGenericError(
+                'Error getting reviews from database',
+            );
+        }
     }
-    return query;
-  }
+
+    async getReviewById(id: string): Promise<MovieReview> {
+        try {
+            const found = await this.movieReviewRepository.findOneBy({ id });
+            if (!found) {
+                throw new RepositoryGenericError(
+                    'Error getting review from database',
+                );
+            }
+            return found;
+        } catch (err) {
+            throw new RepositoryGenericError(
+                'Error getting review from database',
+            );
+        }
+    }
+
+    async deleteReviewById(id: string): Promise<void> {
+        try {
+            const result = await this.movieReviewRepository.delete({ id });
+            if (result.affected === 0) {
+                throw new RepositoryGenericError(
+                    'Error deleting review from database',
+                );
+            }
+            return;
+        } catch (err) {
+            throw new RepositoryGenericError(
+                'Error deleting review from database',
+            );
+        }
+    }
+
+    async updateMovieReviewNotes(
+        id: string,
+        data: UpdateMovieReviewDto,
+    ): Promise<MovieReview> {
+        try {
+            const { notes } = data;
+
+            const found = await this.movieReviewRepository.findOne({
+                where: { id },
+            });
+            if (!found) {
+                throw new RepositoryGenericError(
+                    'Review not found on database',
+                );
+            }
+            found.notes = notes;
+
+            return await this.movieReviewRepository.save(found);
+        } catch (err) {
+            throw new RepositoryGenericError(
+                'Error updating review from database',
+            );
+        }
+    }
+
+    private constructWhere(
+        query: SelectQueryBuilder<MovieReview>,
+        dto: GetMovieReviewsQueryDto,
+    ): SelectQueryBuilder<MovieReview> {
+        const { sortRating, sortRelease, search } = dto;
+
+        if (search) {
+            query.andWhere(
+                'LOWER(movieReviews.title) LIKE LOWER(:search) OR LOWER(movieReviews.actors) LIKE LOWER(:search) OR LOWER(movieReviews.directors) LIKE LOWER(:search)',
+                { search: `%${search}%` },
+            );
+        }
+        if (sortRelease) {
+            query.addOrderBy('movieReviews.released', sortRelease);
+        }
+
+        if (sortRating) {
+            query.addOrderBy('movieReviews.imdbRating', sortRating);
+        }
+        return query;
+    }
 }
