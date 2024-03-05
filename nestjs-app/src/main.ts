5a97a02d9d388bc0e01bef79092ec93bccfd0f4e
Carlos Souza <carlossouzap@outlook.com>
2024-03-05 00:18:53 -0300

    feat: configure swaeger

--- nestjs-app/src/main.ts
+++ nestjs-app/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -12,7 +12,17 @@ async function bootstrap() {
             forbidNonWhitelisted: true,
         }),
     );
+    const config = new DocumentBuilder()
+        .setTitle('Documentação com Swagger - Fábrica de Sinapse')
+        .setDescription(
+            'O Swagger (aka OpenApi) é uma biblioteca muito conhecida no universo backend, estando disponível para diversas linguagens e frameworks. Ela gera um site interno no seu backend que descreve, com muitos detalhes, cada endpoint e estrutura de entidades presentes na sua aplicação.',
+        )
+        .setVersion('1.0')
+        .addTag('movie-reviews')
+        .build();
 
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('docs', app, document);
     await app.listen(3001);
 }
 bootstrap();
